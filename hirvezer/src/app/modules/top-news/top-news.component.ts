29eabe6592d56b729aa9f35941a2be671ea758bd
BSB <hello@ballaschottner.com>
2022-08-20 22:49:38 +0200

    RSS feeds poc

--- hirvezer/src/app/modules/top-news/top-news.component.ts
+++ hirvezer/src/app/modules/top-news/top-news.component.ts
@@ -9,11 +9,45 @@ import { NewsapiService } from 'src/app/service/newsapi.service';
 export class TopNewsComponent implements OnInit {
   constructor(private api: NewsapiService) {}
   topHeadlinesData: any[] = [];
+  hvgData: any[] = [];
+  mergedNews: any[] = [];
+  newsApiData: any[] = [];
 
   ngOnInit(): void {
-    this.api.topHeadlines().subscribe((result) => {
-      console.log(result);
-      this.topHeadlinesData = result.articles;
+    this.api.ZaolNews().subscribe((result) => {
+      console.log(result.items);
+      this.topHeadlinesData = result.items;
+      this.mergedNews.push(...result.items);
     });
+
+    this.api.hvgNews().subscribe((hvgresult) => {
+      console.log('HVG:', hvgresult.items);
+      this.hvgData = hvgresult.items;
+      this.mergedNews.push(...hvgresult.items);
+    });
+
+    this.api.newsApi().subscribe((newsresult) => {
+      console.log('NewsAPI:', newsresult.articles);
+      this.newsApiData = newsresult.items;
+    });
+
+    const result = this.mergedNews.sort(function (a, b) {
+      if (a.pubDate < b.pubDate) {
+        return -1;
+      }
+      if (a.pubDate > b.pubDate) {
+        return 1;
+      }
+      return 0;
+    });
+    console.log('mergedSort', result);
+
+    /*     const sliceResult = this.mergedNews.slice(25);
+     */
+    /*     console.log('merged pubDate', this.mergedNews.pubDate);
+     */
+    /* console.log('mergedNews:', this.mergedNews);
+    console.log('mergedSorted:', result);
+    console.log('mergedSliced:', sliceResult); */
   }
 }
--- hirvezer/src/app/service/newsapi.service.ts
+++ hirvezer/src/app/service/newsapi.service.ts
@@ -8,10 +8,39 @@ import { Observable } from 'rxjs';
 export class NewsapiService {
   constructor(private http: HttpClient) {}
   APIKey = '21c5c042847d414d89dd8ad24744d877';
-  topNews =
-    'https://newsapi.org/v2/top-headlines?country=hu&apiKey=' + this.APIKey;
+  RSSAPi = '8b6960ff147386bf9878f148c7318047';
+  newsAPI =
+    'https://newsapi.org/v2/everything?q=hungary&from=2022-07-20&sortBy=publishedAt&apiKey=' +
+    this.APIKey;
+  ZaolRSS =
+    'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.zaol.hu%2Fpublicapi%2Fhu%2Frss%2Fzaol%2Farticles&api_key=un9axjpfquykmpkwy4rn1nz4lcho4uktdvcohhym&order_by=pubDate&count=100';
+  HVGNews =
+    ' https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fhvg.hu%2Frss&api_key=un9axjpfquykmpkwy4rn1nz4lcho4uktdvcohhym&order_by=pubDate&count=100';
 
-  topHeadlines(): Observable<any> {
-    return this.http.get(this.topNews);
+  newsApi(): Observable<any> {
+    return this.http.get(this.newsAPI);
   }
+
+  ZaolNews(): Observable<any> {
+    return this.http.get(this.ZaolRSS);
+  }
+
+  hvgNews(): Observable<any> {
+    return this.http.get(this.HVGNews);
+  }
+
+  /* customRSSPOC() {
+    const requestOptions: Object = {
+      observe: 'body',
+      responseType: 'text',
+    };
+    this.http
+      .get<any>('https://gadgets.ndtv.com/rss/feeds', requestOptions)
+      .subscribe((data) => {
+        let parseString = xml2js.parseString;
+        parseString(data, (err, result: NewsRss) => {
+          this.RssData = result;
+        });
+      });
+  } */
 }
